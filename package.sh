e1a5459b808f51e2bb3032a2626969a636464c3a
MacMcClain <macmcclain.dev@gmail.com>
2020-05-30 10:33:06 -0700

    added a script to package up a targz of each template to be used by the cli

---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# package up each template into a tar file and put it in the dist folder.
+tar --exclude='./dist' --exclude='./node_modules' -czvf dist/vuejs-starter.tar.gz ./vuejs-starter
